48dc20cedf1a7f4431599755d83a30f4c5df0a20
Phillip Schackier <CppPhil@users.noreply.github.com>
2024-05-11 14:25:22 +0200

    Seems to work

--- build.sh
+++ build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 if [ -d "build" ]; then
   rm -rf build
 fi
 
-cmake -G "Visual Studio 17 2022" -A Win64 -S . -B build
+pushd "$DIR" > /dev/null
+pushd external/matplotlib-cpp > /dev/null
+git apply "$DIR/git_patches/matplotlibcpp_patch.diff"
+popd > /dev/null
+
+cmake -G "Visual Studio 17 2022" -A x64 -S . -B build
 cmake --build build --config Release --parallel 4
+
+pushd external/matplotlib-cpp > /dev/null
+git checkout matplotlibcpp.h
+popd > /dev/null
+popd > /dev/null
+
+exit 0
