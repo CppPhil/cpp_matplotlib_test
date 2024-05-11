2c1ab6ebb1a13c27efd9b6e31e87ec2d74153d14
Phillip Schackier <CppPhil@users.noreply.github.com>
2024-05-11 14:42:51 +0200

    Update

--- build.sh
+++ build.sh
@@ -2,21 +2,29 @@
 
 readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if [ -d "build" ]; then
-  rm -rf build
-fi
-
 pushd "$DIR" > /dev/null
+
+echo "Patching matplotlib-cpp ..."
 pushd external/matplotlib-cpp > /dev/null
 git apply "$DIR/git_patches/matplotlibcpp_patch.diff"
 popd > /dev/null
+echo "Patched matplotlib-cpp."
 
+echo "Generating build files ..."
 cmake -G "Visual Studio 17 2022" -A x64 -S . -B build
-cmake --build build --config Release --parallel 4
+echo "Build files generated."
 
+echo "Building project ..."
+cmake --build build --config Release --parallel 4
+echo "Project built."
+
+echo "Removing matplotlib-cpp patch ..."
 pushd external/matplotlib-cpp > /dev/null
-git checkout matplotlibcpp.h
+git checkout matplotlibcpp.h > /dev/null
 popd > /dev/null
+echo "Removed matplotlib-cpp patch."
+
 popd > /dev/null
+echo "Build completed."
 
 exit 0
